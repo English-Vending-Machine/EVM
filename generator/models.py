b97f643b7fc7eeb39d51ef6c775c12e7042720d8
Donghyun Sohn <bulelion37@naver.com>
2021-05-05 18:19:20 +0900

    Construct DB and connect localhost

---
+++ generator/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class user(models.Model):
+    ID = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=20)
+
+class problem_type(models.Model):
+    type = models.CharField(primary_key=True, max_length=20)
+
+class problem(models.Model):
+    problem_id = models.BigAutoField(primary_key=True)
+    ID = models.ForeignKey(user, on_delete=models.CASCADE)
+    answer = models.IntegerField()
+    type = models.ForeignKey(problem_type, on_delete=models.CASCADE)
+    blank_num = models.IntegerField()
+    image = models.ImageField(null=True, blank=True)
+    text = models.CharField(max_length=2000, blank=True, null=True)
+
