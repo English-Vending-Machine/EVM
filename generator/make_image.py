4a9ceba4e54088308f1a74823587de963392d34e
Donghyun Sohn <bulelion37@naver.com>
2021-05-24 15:15:52 +0900

    make blank problem with info tag

--- generator/make_image.py
+++ generator/make_image.py
@@ -1,20 +1,21 @@
-from io import BytesIO, StringIO
+import os
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
-from django.core.files.base import ContentFile
+import EVM.settings
 
 # 이후 사용 편의를 위하여 함수 형태로 만들었습니다.
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-def make_image(message, problem_id):
+def make_image(message, problem_info):
     # Image size
     W = 640
     H = 400
     bg_color = 'rgb(255, 255, 255)'  # 아이소프트존
 
     # font setting
-    font = ImageFont.truetype('arial.ttf', size=15)
+    fontsFolder = EVM.settings.MEDIA_ROOT
+    font = ImageFont.truetype(os.path.join(fontsFolder,'NanumSquareR.ttf'), size=15)
     font_color = 'rgb(0, 0, 0)'  # or just 'black'
 
     image = Image.new('RGB', (W, H), color=bg_color)
@@ -22,11 +23,15 @@ def make_image(message, problem_id):
 
     # Text wraper to handle long text
     # 40자를 넘어갈 경우 여러 줄로 나눔
-    lines = textwrap.wrap(message, width=60)
+    lines = textwrap.wrap(message, width=70)
 
     # start position for text
     x_text = 50
     y_text = 50
+
+    width, height = font.getsize(problem_info)
+    draw.text((x_text, y_text), problem_info, font = font, fill = font_color)
+    y_text += height*2
 
     # 각 줄의 내용을 적음
     for line in lines:
--- generator/urls.py
+++ generator/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('show_problem', views.show_problem, name='show_problem'),
     path('show_UserProblem', views.show_UserProblem, name='show_UserProblem'),
-    url(r'^upload/$', views.ImageCreateAPIView.as_view()),
     path("<id>/", views.scan_img_from_DB),
 ]
